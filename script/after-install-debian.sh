013ba76f3bead5d8387f6750a96a08224681c7ab
wikinee <neeyongliang@gmail.com>
2018-05-21 20:56:36 +0800

    add go dev build

--- script/after-install-debian.sh
+++ script/after-install-debian.sh
@@ -13,6 +13,7 @@ sudo apt-get update;sudo apt-get upgrade
 echo "Install packages..."
 
 sudo apt-get install zsh git meld devhelp dconf-editor exuberant-ctags \
-    autojump screenfetch htop gitk unrar vim  cmake wps-office lnav
+    autojump clang-format-3.9 clang-3.9 screenfetch htop gitk unrar vim \
+    tree cmake wps-office lnav
 
 echo "May be you also need vscode, sublime, chrome, and so on."
---
+++ script/go-dev-build.sh
@@ -0,0 +1,19 @@
+# !/bin/sh
+# Auhtor:
+#     wikinee
+# Purpose:
+#     This script is for golang dev build.
+# Changelog:
+#     first release 2018/05/21
+
+# git clone https://github.com/golang/tools $GOPATH/golang.org/x/tools
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v github.com/acroca/go-symbols
+go get -u -v github.com/sqs/goreturns
+go get -u -v github.com/golang/lint/golint
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/uudashr/gopkgs
+# go get -u -v github.com
+# go get -u -v github.com
+# go get -u -v github.com
