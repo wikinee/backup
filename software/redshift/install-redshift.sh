36f410e81f507b689609b88fc67ceb3d4b0c4948
wikinee <neeyongliang@gmail.com>
2019-08-01 14:31:33 +0800

    Complet examples

--- software/redshift/install-redshift.sh
+++ software/redshift/install-redshift.sh
@@ -5,16 +5,17 @@
 # 	2019/01/14 wikinee first release
 
 echo -n "Are you need install redshift? (yes/no)"
-read install_choose
+read -r install_choose
 
 case $install_choose in
 	"N"|"NO"|"no"|"No"|"n")
 		echo "Do nothing, exit.";;
 	"Y"|"y"|"Yes"|"yes"|"YES")
-		echo "will install redshift and redshift-gtk";;
+		echo "will install redshift and redshift-gtk"
 		sudo apt install redshift redshift-gtk
-		systemctl enable /usr/lib/systemd/user/redshift.service
-		start redshift.service
+		sudo systemctl enable /usr/lib/systemd/user/redshift.service
+		sudo systemctl start redshift.service
+    ;;
 	*)
 		echo "Do nothing, exit.";;
 esac
