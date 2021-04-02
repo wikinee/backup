61c07f68d64fb39ea2b685ff352ae315e1dd4833
wikinee <neeyongliang@gmail.com>
2021-04-02 23:56:30 +0800

    Add qt developt scripts

--- scripts/after-install-debian.sh
+++ scripts/after-install-debian.sh
@@ -6,14 +6,41 @@
 # may be should install from source if you frequent install system：
 #  vim, cmake, nodejs,
 
-echo " Now, Update & Grade ..."
+echo "###################### Installing ###########################"
 
-sudo apt-get update;sudo apt-get upgrade
+echo "Create directory..."
+mkdir $HOME/Github $HOME/Workspace $HOME/Software
+
+echo "Install chrome..."
+cd $HOME/Softare
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo "Install sublime..."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+echo "Install vscode..."
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+echo " Now, Update & Grade ..."
+sudo apt-get update;
+# sudo apt-get upgrade
 
 echo "Install packages..."
-
 sudo apt-get install zsh git meld devhelp dconf-editor exuberant-ctags \
     autojump screenfetch htop gitk unrar vim tree cmake lnav shellcheck \
-    d-feet apt-transport-https
+    d-feet apt-transport-https screenruler devscripts gnome-common fakeroot \
+    qt5-default python3-pip python3-doc wget curl sublime-text code
 
-echo "May be you also need vscode, sublime, chrome, and so on."
+
+echo "###################### Fonts ###########################"
+cd $HOME/Github
+echo "git clone git@gitee.com:wikinee/custom-fonts.git"
+git clone https://gitee.com/wikinee/backup.git
+
+echo "###################### Scripts  #######################"
+sh $HOME/Github/backup/scripts/install-oh-my-zsh.sh
+sh $HOME/Github/backup/scripts/build-python-dev.sh
+
---
+++ scripts/build-qt-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Program:
+#   Run this shell script to get Qt develop environment.
+# History:
+#   2021/03/31  wikinee First release
+
+set -e
+
+sudo apt install qtcreator qt5-doc-html qt5-assistant qt5-doc qtools5-dev-tools qt5-default
+
